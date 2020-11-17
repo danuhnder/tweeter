46221dfb9a93f970f02f4da4bb6ae7e430294db5
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-17 21:05:21 +0000

    functioning, color changing character counter implemented. HTML framework for tweet display added

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
 
   $("#tweet-text").on('input', function() {
   const tweetLength = $(this).val().length;
-  console.log(tweetLength);
+  const counter = $(this).siblings("div").children("output");
+  counter.val(140 - tweetLength);
+  if (counter.val() < 0) {
+    counter.css({color: 'red'});
+  } else {
+    counter.css({color: 'black'});
+  };
   });
 
 });
