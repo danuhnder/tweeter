a849069bae2518698a7b62470cb813309c2ccb8c
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-17 22:04:30 +0000

    some css selectors removed and JS changes after code review

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
   $("#tweet-text").on('input', function() {
   const tweetLength = $(this).val().length;
-  const counter = $(this).siblings("div").children("output");
+  const counter = $(this).siblings().children("output");
   counter.val(140 - tweetLength);
   if (counter.val() < 0) {
-    counter.css({color: 'red'});
+    counter.addClass('red');
   } else {
-    counter.css({color: 'black'});
+    counter.removeClass('red');
   };
   });
 
