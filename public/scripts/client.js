8c87d21e2a9e0aeebdf4553a9e60dae8c1e81208
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-19 21:26:24 +0000

    responsive design functionality implemented, date function implemented, tidying up code prior to submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,3 @@
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -7,15 +6,28 @@
 
 $(document).ready(function(){
  
+  // Handles dropdown of New Tweet form
   $(".write-focus").on("click", function () {
-    if ($(".new-tweet").first().is(":hidden")) {
-      $(".new-tweet").slideDown(400);
-      $("#tweet-text").focus()
-    } else {
-      $(".new-tweet").slideUp(400);
-    }
+    $(".new-tweet").slideToggle(400);
+    $("#tweet-text").focus();
   });
 
+  // generates readable date from timestamp
+  const dateFunction = (date) => {
+    const newDate = new Date(date)
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const weekday = days[newDate.getDay()];
+    const day = newDate.getDate();
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const month = months[newDate.getMonth()];
+    const year = newDate.getFullYear();
+    const hour = newDate.getHours();
+    const minute = (newDate.getMinutes()<10 ? '0' : '' ) + newDate.getMinutes();
+    const readableDate = `${hour}:${minute} | ${weekday} ${day}-${month}-${year}`;
+    return readableDate;
+  };
+
+  // Creates HTML encoded tweet from object
   const createTweetElement = function(tweet) {
     const $tweet = $(`
     <article class="tweet">
@@ -28,7 +40,7 @@ $(document).ready(function(){
       </header>
       <p>${tweet.content.text}</p>
       <footer>
-        <span>${tweet.created_at}</span>
+        <span>${dateFunction(tweet.created_at)}</span>
         <span>icons</span>
       </footer>
     </article> 
@@ -36,46 +48,66 @@ $(document).ready(function(){
     return $tweet;   
   };
   
+  // takes array of tweet objects, appends HTML version to tweet container
   const renderTweets = function(array) {
     for (let tweet of array) {
       const $tweet = createTweetElement(tweet);
       $(".show-tweets").append($tweet);
     }
   };
+  // tweet composer character counter
+  $("#tweet-text").on('input', function() {
+    const tweetLength = $(this).val().length;
+    const counter = $(this).siblings().children("output");
+    counter.val(140 - tweetLength);
+    if (counter.val() < 0) {
+      counter.addClass('red');
+    } else {
+      counter.removeClass('red');
+    };
+    });
+  
 
+  // protects from cross-site-scripting and enemy hackermans
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
 
+  // submission and error-handling logic
   $("#tweet-form").on("submit", function (event) {
     event.preventDefault();
     const rawHTML = $("#tweet-text").val();
+    // string length checker
     if (rawHTML === null || rawHTML === '' || rawHTML.length > 140) {
       $("#error-msg").show(200);
       setTimeout( () => $("#error-msg").hide(200), 3600);
     } else {
       $("#error-msg").hide(200)
+      // sanitises input before submission
       const safeHTML = escape(rawHTML);
       $.ajax("/tweets/", {
         method: "POST",
         data: {text: safeHTML}
       })
-      .then(
-        () => loadTweets(renderTweets),
-        $("#tweet-text").val("")
-      );
+      .then( () => {
+        // empties the tweet container before refreshing 
+        $(".show-tweets").empty();
+        loadTweets(renderTweets);
+        $("#tweet-text").val("");
+      });
     };
   });
 
+  // fills the tweet bucket up
   const loadTweets = (done) => {
     $.ajax("/tweets/", {
       method: "GET"
     })
     .then(res => done(res));
   }
-
+  // on page load
     loadTweets(renderTweets)
 });
 
